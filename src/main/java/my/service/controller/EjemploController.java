e54fd7921af4550190dad3b510bbcd31a21241cb
root <arellano.gustavo@gmail.com>
2020-06-03 09:55:30 -0500

    Adding more comments to the Controller
    Servidor de pruebas local:
    npm install -g aws-sam-local
    local start-api --template sam.yaml

--- src/main/java/my/service/controller/EjemploController.java
+++ src/main/java/my/service/controller/EjemploController.java
@@ -26,11 +26,14 @@ public class EjemploController {
   https://aws.amazon.com/es/blogs/opensource/java-apis-aws-lambda/
   
   
+npm install -g aws-sam-local
+local start-api --template sam.yaml
 
 mvn clean package
 aws cloudformation package --template-file sam.yaml --output-template-file output-sam.yaml --s3-bucket ultra-media
 aws cloudformation deploy  --template-file output-sam.yaml --stack-name ServerlessSpringbootApi --capabilities CAPABILITY_IAM
 aws cloudformation describe-stacks --stack-name ServerlessSpringbootApi --query 'Stacks[0].Outputs[*].{Service:OutputKey,Endpoint:OutputValue}'
 curl -s https://s7539ax3bd.execute-api.us-east-1.amazonaws.com/Prod/prueba?id=97
+
 
 */
